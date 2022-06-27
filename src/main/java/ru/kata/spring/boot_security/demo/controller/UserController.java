aa9ac100e10cf2eae8d9c696c9edaa0ae7d9015c
Alexander735 <happylesa@icloud.com>
2022-06-27 20:52:12 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,69 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/")
+    public String welcome() {
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "users")
+    public String allUsers(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping(value = "users/add")
+    public String addUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "addUser";
+    }
+
+    @PostMapping(value = "users/add")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "users/edit/{id}")
+    public String editUser(ModelMap model, @PathVariable("id") Long id) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "editUser";
+    }
+
+    @PostMapping(value = "users/edit")
+    public String edit(@ModelAttribute("user") User user) {
+        userService.editUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("users/delete")
+    public String deleteUserById(@RequestParam("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("users/{id}")
+    public String show(@PathVariable("id") Long id, ModelMap modelMap) {
+        modelMap.addAttribute("user", userService.getUserById(id));
+        return "show";
+    }
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,48 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Transactional
+    @Override
+    public void editUser(User user) {
+        userDao.editUser(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+}
