4fd0ea08f7aa4e9f0cc4834fe913e2c29e9480cd
Alexander735 <happylesa@icloud.com>
2022-06-28 19:14:09 +0300

    Controllers & templates added

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -17,53 +17,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/")
-    public String welcome() {
-        return "redirect:/users";
+    @GetMapping("/user")
+    public String userPage(Model model) {
+        model.addAttribute(userService.getCurrentUser());
+        return "user";
     }
-
-    @GetMapping(value = "users")
-    public String allUsers(ModelMap model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "users";
-    }
-
-    @GetMapping(value = "users/add")
-    public String addUser(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "addUser";
-    }
-
-    @PostMapping(value = "users/add")
-    public String addUser(@ModelAttribute("user") User user) {
-        userService.addUser(user);
-        return "redirect:/";
-    }
-
-    @GetMapping(value = "users/edit/{id}")
-    public String editUser(ModelMap model, @PathVariable("id") Long id) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        return "editUser";
-    }
-
-    @PostMapping(value = "users/edit")
-    public String edit(@ModelAttribute("user") User user) {
-        userService.editUser(user);
-        return "redirect:/";
-    }
-
-    @GetMapping("users/delete")
-    public String deleteUserById(@RequestParam("id") Long id) {
-        userService.deleteUser(id);
-        return "redirect:/";
-    }
-
-    @GetMapping("users/{id}")
-    public String show(@PathVariable("id") Long id, ModelMap modelMap) {
-        modelMap.addAttribute("user", userService.getUserById(id));
-        return "show";
-    }
-
 }
